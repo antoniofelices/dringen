7123824a7ae8034063570684a438214e471a42f6
antoniofelices <antonio@studiomoare.com>
2025-07-28 18:19:42 +0200

    Add access to the profile in the header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -13,12 +13,7 @@ const Header = () => {
         <nav className=" border-b border-gray-200 px-4 py-2.5 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
             <div className="flex flex-wrap justify-between items-center">
                 <div className="flex justify-start items-center">
-                    <button
-                        data-drawer-target="drawer-navigation"
-                        data-drawer-toggle="drawer-navigation"
-                        aria-controls="drawer-navigation"
-                        className="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    >
+                    <button className="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <svg
                             aria-hidden="true"
                             className="w-6 h-6"
@@ -57,54 +52,17 @@ const Header = () => {
                     </Link>
                 </div>
                 <div className="flex items-center lg:order-2">
-                    <button
-                        type="button"
+                    <Link
+                        to="/user/profile"
                         className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                        id="user-menu-button"
-                        aria-expanded="false"
-                        data-dropdown-toggle="dropdown"
+                        aria-label="access to your profile"
                     >
-                        <span className="sr-only">Open user menu</span>
                         <img
                             className="w-8 h-8 rounded-full"
                             src="https://i1.sndcdn.com/avatars-000105458287-8bilp3-t120x120.jpg"
                             alt="user photo"
                         />
-                    </button>
-                    <div
-                        className="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
-                        id="dropdown"
-                    >
-                        <div className="py-3 px-4">
-                            <span className="block text-sm font-semibold text-gray-900 dark:text-white">
-                                Lorem Ipsum
-                            </span>
-                            <span className="block text-sm text-gray-900 truncate dark:text-white">
-                                name@nf.com
-                            </span>
-                        </div>
-                        <ul
-                            className="py-1 text-gray-700 dark:text-gray-300"
-                            aria-labelledby="dropdown"
-                        >
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                                >
-                                    My profile
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                                >
-                                    Account settings
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </nav>
