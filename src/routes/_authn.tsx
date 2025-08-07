476a1566e5608f3269e5a550dcaf978a1ef505a1
antoniofelices <antonio@studiomoare.com>
2025-08-07 19:22:23 +0200

    Set up redirection to the dashboard when a user logs in

--- src/routes/_authn.tsx
+++ src/routes/_authn.tsx
@@ -1,7 +1,14 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router'
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
+import { supabase } from '@/services/supabaseService'
 import Main from '@layouts/authn/Main'
 
 export const Route = createFileRoute('/_authn')({
+    beforeLoad: async () => {
+        const { data } = await supabase.auth.getSession()
+        if (data.session) {
+            throw redirect({ to: '/dashboard' })
+        }
+    },
     component: RouteComponent,
 })
 
