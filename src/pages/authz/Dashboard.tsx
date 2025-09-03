c2717855f87357f010dcc5d0a416b42fa81172c0
antoniofelices <antonio@studiomoare.com>
2025-09-03 12:31:48 +0200

    Improve dashboard screen

--- src/pages/authz/Dashboard.tsx
+++ src/pages/authz/Dashboard.tsx
@@ -1,10 +1,39 @@
+import { useCurrentUser } from '@hooks/useCurrentUser'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/base/card'
+import ContentArticle from '@components/ui/ContentArticle'
+import Loading from '@components/ui/Loading'
+import ErrorApi from '@components/ui/ErrorApi'
 import content from '@/config/data/pages/dashboard'
 
 const Dashboard = () => {
+    const { user, isPending, isError, error } = useCurrentUser()
+
+    if (isPending) return <Loading />
+    if (isError && error) return <ErrorApi message={error.message} />
+
     return (
-        <>
-            <h1 className="mb-8">{content.title}</h1>
-        </>
+        <ContentArticle>
+            <div className="grid grid-cols-2 gap-6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>
+                            <h1>
+                                {content.title} {user?.user_name}{' '}
+                                {user?.user_last_name}
+                            </h1>
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <p>{content.textWelcome}</p>
+                    </CardContent>
+                </Card>
+            </div>
+        </ContentArticle>
     )
 }
 
