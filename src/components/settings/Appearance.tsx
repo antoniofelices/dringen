0100db4e2dda0ee2bb9f145a4311ddc9994d1f69
antoniofelices <antonio@studiomoare.com>
2025-09-08 12:22:40 +0200

    Add images preview, dark light mode

--- src/components/settings/Appearance.tsx
+++ src/components/settings/Appearance.tsx
@@ -5,6 +5,9 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/base/card'
+import dark from '@/assets/images/dark.webp'
+import light from '@/assets/images/light.webp'
+import system from '@/assets/images/system.svg'
 import content from '@data/settings/appearance'
 
 const Appearance = () => {
@@ -18,10 +21,10 @@ const Appearance = () => {
                 </CardTitle>
             </CardHeader>
             <CardContent>
-                <div className="grid grid-cols-3 gap-6">
+                <div className="grid grid-cols-3 gap-8">
                     <div
                         onClick={() => setTheme('light')}
-                        className="rounded-lg cursor-pointer bg-white dark:bg-blue-800"
+                        className="rounded-lg cursor-pointer"
                         tabIndex={0}
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' || e.key === ' ') {
@@ -29,11 +32,16 @@ const Appearance = () => {
                             }
                         }}
                     >
-                        {content.textLight}
+                        <img
+                            src={light}
+                            alt={content.altImageLight}
+                            className="blur-[1px]"
+                        />
+                        <p className="font-black mt-4">{content.textLight}</p>
                     </div>
                     <div
                         onClick={() => setTheme('dark')}
-                        className="rounded-lg cursor-pointer bg-white dark:bg-blue-800"
+                        className="rounded-lg cursor-pointer"
                         tabIndex={0}
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' || e.key === ' ') {
@@ -41,11 +49,16 @@ const Appearance = () => {
                             }
                         }}
                     >
-                        {content.textDark}
+                        <img
+                            src={dark}
+                            alt={content.altImageDark}
+                            className="blur-[1px]"
+                        />
+                        <p className="font-black mt-4">{content.textDark}</p>
                     </div>
                     <div
                         onClick={() => setTheme('system')}
-                        className="rounded-lg cursor-pointer bg-white dark:bg-blue-800"
+                        className="rounded-lg cursor-pointer"
                         tabIndex={0}
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' || e.key === ' ') {
@@ -53,7 +66,12 @@ const Appearance = () => {
                             }
                         }}
                     >
-                        {content.textSystem}
+                        <img
+                            src={system}
+                            alt={content.altImageSystem}
+                            className="blur-[1px]"
+                        />
+                        <p className="font-black mt-4">{content.textSystem}</p>
                     </div>
                 </div>
             </CardContent>
