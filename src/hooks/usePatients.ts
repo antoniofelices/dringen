ea7ba96f6f2a6da9037534d99765ff3ba0b6c6e5
antoniofelices <antonio@studiomoare.com>
2025-09-14 19:30:21 +0200

    Add hooks user

--- src/hooks/usePatients.ts
+++ src/hooks/usePatients.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getPatients } from '@services/supabaseService'
 
@@ -20,4 +21,22 @@ export const usePatients = () => {
         error: patientsErrorType,
         refetch: userRefetch,
     }
+}
+
+export const usePatientsNames = () => {
+    const { patients } = usePatients()
+
+    const patientsData = useMemo(() => {
+        return (
+            patients
+                ?.map((item) => ({
+                    name: `${item.user_last_name} ${item.user_name}`,
+                    id: item.id,
+                    dni: item.dni,
+                }))
+                .sort((a, b) => a.name.localeCompare(b.name)) || []
+        )
+    }, [patients])
+
+    return patientsData
 }
