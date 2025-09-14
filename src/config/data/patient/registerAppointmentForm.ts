9ae4a70c5d9542ce243cf5131dda1feeb56cbf04
antoniofelices <antonio@studiomoare.com>
2025-09-14 19:31:37 +0200

    Add translations

---
+++ src/config/data/patient/registerAppointmentForm.ts
@@ -0,0 +1,11 @@
+const content = {
+    title: 'Register Appointment Form',
+    labelPatient: 'Patient',
+    labelPhysician: 'Physician',
+    labelDate: 'Appointment date',
+    labelNotes: 'Notes',
+    textButtonSend: 'Register appointment',
+    textButtonSending: 'Sending',
+}
+
+export default content
