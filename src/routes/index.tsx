a5dcf08a3b62b77612985f2d4ede93ec72703d3b
antoniofelices <antonio@studiomoare.com>
2026-01-30 16:05:42 +0100

    WIP: Remove Supabase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
-import { supabase } from '@/services/supabaseService'
 import Index from '@pages/Index.tsx'
 
 export const Route = createFileRoute(`/`)({
     beforeLoad: async () => {
-        const { data } = await supabase.auth.getSession()
-        if (data.session) {
-            throw redirect({ to: '/dashboard' })
-        }
+        // const { data } = await null
+        // if (data.session) {
+        //     throw redirect({ to: '/dashboard' })
+        // }
     },
     component: RouteComponent,
 })
