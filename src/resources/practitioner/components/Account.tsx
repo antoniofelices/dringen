bf6a037851921ebc5f2c264e2f916bb99422ce18
antoniofelices <antonio@studiomoare.com>
2026-02-05 05:18:39 +0100

    Remove reset password card

--- src/resources/practitioner/components/Account.tsx
+++ src/resources/practitioner/components/Account.tsx
@@ -1,11 +1,9 @@
 import AccountDetails from '@resources/practitioner/components/AccountDetails'
-import AccountResetPassword from '@resources/practitioner/components/AccountResetPassword'
 
 const Account = () => {
     return (
         <div className="grid grid-cols-2 gap-6">
             <AccountDetails />
-            <AccountResetPassword />
         </div>
     )
 }
--- src/routes/_authz/practitioner/$id.tsx
+++ src/routes/_authz/practitioner/$id.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import SinglePractitioner from '@/pages/practitioner/SinglePractitioner'
-import ProtectedRoute from '@/components/ProtectedRoute'
+import SinglePractitioner from '@resources/practitioner/pages/Single'
+// import ProtectedRoute from '@shared/components/ProtectedRoute'
 
 export const Route = createFileRoute('/_authz/practitioner/$id')({
     component: RouteComponent,
@@ -13,9 +13,5 @@ export const Route = createFileRoute('/_authz/practitioner/$id')({
 
 function RouteComponent() {
     const { id } = Route.useParams()
-    return (
-        <ProtectedRoute allowedRoles={['admin']}>
-            <SinglePractitioner id={id} />
-        </ProtectedRoute>
-    )
+    return <SinglePractitioner id={id} />
 }
