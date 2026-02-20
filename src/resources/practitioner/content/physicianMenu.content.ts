70f989593d90194de971f29b0ad8b1f3a3ec8c10
antoniofelices <antonio@studiomoare.com>
2026-02-20 02:35:33 +0100

    Add dynamic URLs to the practitioner menu

--- src/resources/practitioner/content/physicianMenu.content.ts
+++ src/resources/practitioner/content/physicianMenu.content.ts
@@ -3,25 +3,25 @@ import { CalendarDays, ClipboardPlus, PlusCircleIcon } from 'lucide-react'
 
 export const physicianMenuTitle = 'Menu Physician'
 
-export const physicianMenu: RoutesProps[] = [
+export const physicianMenu = (practitionerId: string): RoutesProps[] => [
     {
         id: 1,
         icon: CalendarDays,
-        url: '/practitioner/VARIABLE/today',
+        url: `/practitioner/${practitionerId}/today`,
         text: 'Today',
         orderMenu: 1,
     },
     {
         id: 2,
         icon: ClipboardPlus,
-        url: '/practitioner/VARIABLE/schedule',
+        url: `/practitioner/${practitionerId}/schedule`,
         text: 'My Schedule',
         orderMenu: 2,
     },
     {
         id: 3,
         icon: ClipboardPlus,
-        url: `/practitioner/VARIABLE/patient-list`,
+        url: `/practitioner/${practitionerId}/patient-list`,
         text: 'My Patients',
         orderMenu: 3,
     },
