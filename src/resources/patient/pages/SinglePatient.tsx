165a4fd8560b9bc9647dd87963d28acb88cfe647
antoniofelices <antonio@studiomoare.com>
2026-02-24 19:19:24 +0100

    Move clinical encounter drawer to list component, remove ClinicalEncounterDrawer component

--- src/resources/patient/pages/SinglePatient.tsx
+++ src/resources/patient/pages/SinglePatient.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import RoleGuard from '@auth/components/RoleGuard'
 import { getAgeFromDate } from '@shared/utils/utils'
-import { Button } from '@shared/components/ui/base/button'
-import { Drawer, DrawerTrigger } from '@shared/components/ui/base/drawer'
 import ButtonBack from '@shared/components/ui/ButtonBack'
 import ContentArticle from '@shared/components/ui/ContentArticle'
 import HeaderArticle from '@shared/components/ui/HeaderArticle'
@@ -10,15 +7,11 @@ import { useSinglePatient } from '@resources/patient/hooks/useGetPatient'
 import PatientDemographics from '@resources/patient/components/PatientDemographics/PatientDemographics'
 import AllergyIntoleranceList from '@resources/allergy-intolerance/components/AllergyIntoleranceList'
 import FamilyMemberHistoryList from '@resources/family-member-history/components/FamilyMemberHistoryList'
-import {
-    ClinicalEncounterDrawer,
-    EncounterList,
-} from '@workflows/clinical-encounter/index'
+import { EncounterList } from '@workflows/clinical-encounter/index'
 import content from './SinglePatient.content'
 
 const SinglePatient = ({ id }: { id: string }) => {
     const { patient } = useSinglePatient(id)
-    const [isEncounterDrawerOpen, setIsEncounterDrawerOpen] = useState(false)
 
     if (!patient) return null
 
@@ -26,28 +19,7 @@ const SinglePatient = ({ id }: { id: string }) => {
         <>
             <HeaderArticle
                 title={`${patient.firstName} - ${getAgeFromDate(patient.birthDate)} ${content.textYears}`}
-            >
-                <RoleGuard allowedRoles={['doctor']}>
-                    <div>
-                        <Drawer
-                            open={isEncounterDrawerOpen}
-                            onOpenChange={setIsEncounterDrawerOpen}
-                        >
-                            <Button asChild size="sm">
-                                <DrawerTrigger>
-                                    {content.textButtonAddEncounter}
-                                </DrawerTrigger>
-                            </Button>
-                            <ClinicalEncounterDrawer
-                                patientId={id}
-                                onSuccess={() =>
-                                    setIsEncounterDrawerOpen(false)
-                                }
-                            />
-                        </Drawer>
-                    </div>
-                </RoleGuard>
-            </HeaderArticle>
+            ></HeaderArticle>
             <ContentArticle>
                 <div className="grid lg:grid-cols-6 gap-6 place-content-between">
                     <div className="col-span-3">
--- src/workflows/clinical-encounter/components/ClinicalEncounterDrawer.content.tsx
+++
@@ -1,6 +0,0 @@
-const content = {
-    textPresentIllness: 'History of Present Illness',
-    textPresentIllnessForm: 'A History of Present Illness form',
-}
-
-export default content
