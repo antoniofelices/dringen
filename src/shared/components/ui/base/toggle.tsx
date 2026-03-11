8919f58a621aea5a6f5806b1a857e23ef4c3b778
antoniofelices <antonio@studiomoare.com>
2026-03-11 10:38:00 +0100

    Add Toggle Shadcn component

---
+++ src/shared/components/ui/base/toggle.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+import { Toggle as TogglePrimitive } from 'radix-ui'
+
+import { cn } from '@shared/utils/utils'
+
+const toggleVariants = cva(
+    "inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none hover:bg-gray-100 hover:text-gray-500 focus-visible:border-gray-950 focus-visible:ring-[3px] focus-visible:ring-gray-950/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-red-500 aria-invalid:ring-red-500/20 data-[state=on]:bg-gray-100 data-[state=on]:text-gray-900 dark:aria-invalid:ring-red-500/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 dark:hover:bg-gray-800 dark:hover:text-gray-400 dark:focus-visible:border-gray-300 dark:focus-visible:ring-gray-300/50 dark:aria-invalid:border-red-900 dark:aria-invalid:ring-red-900/20 dark:data-[state=on]:bg-gray-800 dark:data-[state=on]:text-gray-50 dark:dark:aria-invalid:ring-red-900/40",
+    {
+        variants: {
+            variant: {
+                default: 'bg-transparent',
+                outline:
+                    'border border-gray-200 bg-transparent shadow-xs hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-50',
+            },
+            size: {
+                default: 'h-9 min-w-9 px-2',
+                xs: 'h-6 min-w-6 px-1',
+                sm: 'h-8 min-w-8 px-1.5',
+                lg: 'h-10 min-w-10 px-2.5',
+            },
+        },
+        defaultVariants: {
+            variant: 'default',
+            size: 'default',
+        },
+    }
+)
+
+function Toggle({
+    className,
+    variant,
+    size,
+    ...props
+}: React.ComponentProps<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants>) {
+    return (
+        <TogglePrimitive.Root
+            data-slot="toggle"
+            className={cn(toggleVariants({ variant, size, className }))}
+            {...props}
+        />
+    )
+}
+
+export { Toggle, toggleVariants }
