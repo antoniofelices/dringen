b78ddb6f9ac029d01037f767cce4e0e83f338672
antoniofelices <antonio@studiomoare.com>
2026-04-17 21:55:42 +0200

    Remove dead const, add snomed specialty

--- src/resources/practitioner/domain/practitioner.domain.ts
+++ src/resources/practitioner/domain/practitioner.domain.ts
@@ -8,3 +8,11 @@ export const ROLE_PRACTITIONER_TO_SNOMED: Record<
     administrative: ['224608005', 'Administrative worker'],
     'administrative-hr': ['224608005', 'Administrative worker'],
 }
+
+export const SPECIALTY_TO_SNOMED = {
+    pediatric: ['394537008', 'Pediatric specialty'],
+    'obstetrics-and-gynecology': ['394585009', 'Obstetrics and gynecology'],
+    'internal-medicine': ['419192003', 'Internal medicine'],
+    'surgery-general': ['394609007', 'Surgery-general'],
+    dermatology: ['394582007', 'Dermatology'],
+}
